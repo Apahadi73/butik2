a0eebe86fdca00eeb0261a94f8805eae14149f2f
Amir Pahadi <apahadi73@gmail.com>
2021-11-07 16:27:55 -0600

    app themes setup complete

---
+++ src/infrastructures/themes/Spacing.themes.ts
@@ -0,0 +1,10 @@
+export interface LineHeight {
+  title: string;
+  copy: string;
+}
+export const lineHeights: LineHeight = {
+  title: "28px",
+  copy: "20px",
+};
+
+export const space: string[] = ["0px", "4px", "8px", "16px", "32px", "64px"];
--- src/infrastructures/themes/index.ts
+++ src/infrastructures/themes/index.ts
@@ -1,13 +1,35 @@
 // import { DefaultTheme } from "styled-components/native";
 import { DefaultTheme } from "styled-components/native";
+import {
+  fonts,
+  Fonts,
+  fontSizes,
+  FontSizes,
+  fontWeights,
+  FontWeights,
+} from "./Fonts.themes";
+import { sizes } from "./Size.themes";
+import { LineHeight, lineHeights, space } from "./Spacing.themes";
 import { themeColor, ThemeColor } from "./ThemeColor.themes";
 
 declare module "styled-components" {
   export interface DefaultTheme {
     themeColor: ThemeColor;
+    space: string[];
+    lineHeights: LineHeight;
+    sizes: string[];
+    fonts: Fonts;
+    fontSizes: FontSizes;
+    fontWeights: FontWeights;
   }
 }
 
 export const theme: DefaultTheme = {
   themeColor: themeColor,
+  space: space,
+  lineHeights: lineHeights,
+  sizes: sizes,
+  fonts: fonts,
+  fontSizes: fontSizes,
+  fontWeights: fontWeights,
 };
