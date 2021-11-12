0c15e758ef6cb579ba58c2430cfcd32c0a0c746a
Amir Pahadi <apahadi73@gmail.com>
2021-11-12 17:49:44 -0600

    backend directory structure setup

--- backend/pkg/http/rest/handler.go
+++
@@ -1,9 +0,0 @@
-package rest
-
-import "github.com/gorilla/mux"
-
-func InitHandlers() *mux.Router  {
-	router:= mux.NewRouter()
-	router.HandleFunc("/api/",welcoleHandler).Methods(("GET"))
-	return router
-}
