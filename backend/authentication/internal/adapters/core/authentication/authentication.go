eda05cedfe025897d1273b7ff6214a85bd00ff30
Amir Pahadi <apahadi73@gmail.com>
2021-11-14 04:59:28 -0600

    setting up dockerfile

--- backend/authentication/internal/adapters/core/authentication/authentication.go
+++ backend/authentication/internal/adapters/core/authentication/authentication.go
@@ -10,25 +10,15 @@ func NewAdapter() *Adapter{
 	return &Adapter{}
 }
 
-// func (auth Adapter) Register(email string, password string) (string,error){
-// 	isValid := isEmailValid(email)
-// 	if (isValid){
-// 		return 
-// 	}
-// 	return "New user registered",nil;
-// }  
-
-
-// func (auth Adapter) Authenticate(uPassword , dbPassword string) (string,error){
-// 	if uPassword == dbPassword{
-// 		return "User authenticated",nil;
-
-// 	}
-// 	return "Password does not match",nil;
-// }  
-
 //  checks whether the email is valid or not
 func (auth Adapter) CheckEmailValidity(email string) bool {
     _, err := mail.ParseAddress(email)
     return err == nil
 }
+
+// //  generates a new jwt token and returns the token
+// func (auth Adapter) GenerateToken(email string, password string) (string,error) {
+//     _, err := mail.ParseAddress(email)
+//     return err == nil
+// }
+
