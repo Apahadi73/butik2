1f2065aff10c37681c39207d7fafeea828fa9e4b
apahadi73 <apahadi73@gmail.com>
2021-11-13 23:43:58 -0600

    fiber v2 used for the server

--- backend/authentication/internal/adapters/framework/left/http_server/handlers/handlers.go
+++ backend/authentication/internal/adapters/framework/left/http_server/handlers/handlers.go
@@ -1,15 +1,15 @@
 package handlers
 
 import (
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 )
 
 // registers new user
-func Register(c *fiber.Ctx) {
-	 c.SendString("Hello, World 👋! for the v1 register")
+func Register(c *fiber.Ctx) error{
+	 return c.SendString("Hello, World 👋! for the v1 register")
 }
 
 // login already existing user
-func  Login(c *fiber.Ctx) {
-	 c.SendString("Hello, World 👋! for the v1 register")
+func  Login(c *fiber.Ctx) error {
+	 return c.SendString("Hello, World 👋! for the v1 register")
 }
--- backend/authentication/internal/adapters/framework/left/http_server/routes/routes.go
+++ backend/authentication/internal/adapters/framework/left/http_server/routes/routes.go
@@ -3,7 +3,7 @@ package routes
 import (
 	"butik/backend/authentication/internal/adapters/framework/left/http_server/handlers"
 
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 )
 
 // sets up all the route handlers
