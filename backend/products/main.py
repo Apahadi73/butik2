cce85582168187973f3fbeca8b7ff88e1d81f0b6
apahadi73 <apahadi73@gmail.com>
2021-11-14 22:46:17 -0600

    working on read module

--- backend/products/main.py
+++ backend/products/main.py
@@ -3,9 +3,10 @@ from typing import Optional
 from pydantic import BaseModel
 from dummy_data import products
 from src.routers import products
+from src.read.router import read_router
 
 # create our application
 app = FastAPI(title="Products Service")
 
 # sets up router
-app.include_router(products.router)
+app.include_router(read_router.router)
--- backend/products/src/routers/products.py
+++ backend/products/src/routers/products.py
@@ -1,45 +1,46 @@
-from fastapi import FastAPI, Path, APIRouter
-from fastapi_versioning import VersionedFastAPI, version
-from typing import Optional
-from pydantic import BaseModel
-from src.dummy_data import products
-from src.models.product import Product
+# from fastapi import FastAPI, Path, APIRouter
+# from fastapi_versioning import VersionedFastAPI, version
+# from typing import Optional
+# from pydantic import BaseModel
+# from src.dummy_data import products
+# from src.models.product import Product
+# from src
     
-# creates router
-router = APIRouter(
-    prefix="/api/v1",
-    tags=["v1"],
-    responses={404: {"description": "Not found"}},
-)
+# # creates router
+# router = APIRouter(
+#     prefix="/api/v1",
+#     tags=["v1"],
+#     responses={404: {"description": "Not found"}},
+# )
 
-@router.get("/products/")
-def get_products():
-    return products.products
+# @router.get("/products/")
+# def get_products():
+#     get_products_api()
 
-@router.get("/products/{id}")
-def get_products_by_id(id:int = Path(None,description="Product Id",gt=0)):
-    response = products.products[id]
-    return response
+# @router.get("/products/{id}")
+# def get_products_by_id(id:int = Path(None,description="Product Id",gt=0)):
+#     get_products_by_id(id)
+    
 
-@router.get("/products/product-name/")
-async def get_products_by_name(*,name: Optional[str] = None):
-    for product_id in products.products:
-        if products.products[product_id]["name"] == name:
-            return  products.products[product_id]
-    return {"Data":"Not Found"}
-
-
-@router.post("/products/")
-async def create_product(product:Product):
-    products.products[19]=product
-    return products.products
+# @router.get("/products/product-name/")
+# async def get_products_by_name(*,name: Optional[str] = None):
+#     for product_id in products.products:
+#         if products.products[product_id]["name"] == name:
+#             return  products.products[product_id]
+#     return {"Data":"Not Found"}
 
 
-@router.put("/products/{id}")
-async def update_product(id:int,product:Product):
-    if id not in products.products:
-        return {"Data":"Not Found"}
-    else:
-        products.products[id]=product
-        return products.products[id]
+# @router.post("/products/")
+# async def create_product(product:Product):
+#     products.products[19]=product
+#     return products.products
+
+
+# @router.put("/products/{id}")
+# async def update_product(id:int,product:Product):
+#     if id not in products.products:
+#         return {"Data":"Not Found"}
+#     else:
+#         products.products[id]=product
+#         return products.products[id]
     
