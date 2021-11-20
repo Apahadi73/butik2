c1249b9e0e9ec8699d2942cb0d55770672dae300
Amir Pahadi <apahadi73@gmail.com>
2021-11-20 01:01:52 -0600

    orders service added

--- backend/products/src/create/application/create_api.py
+++
@@ -1,6 +0,0 @@
-from ..repo.models.product import Product
-from ..repo.dummy_data.products import products
-
-async def create_product(product:Product):
-    products[19]=product
-    return products[19]
