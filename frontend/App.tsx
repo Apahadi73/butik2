161898fa8ee866fc008d0d92829fcd16e97237d4
Amir Pahadi <apahadi73@gmail.com>
2021-11-21 17:41:50 -0600

    butik added

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { ThemeProvider } from "styled-components/native";
-import { AppContainer } from "./src/components/AppContainer.component";
-import NavigationHost, {
-  Navigation,
-} from "./src/infrastructures/navigation_copy";
-import { theme } from "./src/infrastructures/themes";
-import AuthenticationContextProvider from "./src/services/authentication/repo/Authentication.context";
-import LoginScreen from "./src/services/authentication/screens/LoginScreen.authentication";
-import RegisterScreen from "./src/services/authentication/screens/RegisterScreen.authentication";
+import { useFonts } from "expo-font";
 import {
   Lato_400Regular,
   Lato_400Regular_Italic,
   Lato_700Bold,
   Lato_700Bold_Italic,
 } from "@expo-google-fonts/lato";
-import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "styled-components/native";
+
+import Navigation from "../butik/src/infrastructure/navigation";
+import { theme } from "../butik/src/infrastructure/themes";
+import AuthenticationContextProvider from "../butik/src/services/authentication/repo/Authentication.context";
 import AppLoading from "expo-app-loading";
+import { AppContainer } from "../butik/src/components/AppContainer.component";
 
 export default function App() {
   let [fontsLoaded, error] = useFonts({
@@ -30,12 +29,13 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <AppContainer>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
         <AuthenticationContextProvider>
-          <NavigationHost />
+          <Navigation />
         </AuthenticationContextProvider>
-      </AppContainer>
-    </ThemeProvider>
+      </ThemeProvider>
+      <StatusBar />
+    </SafeAreaProvider>
   );
 }
