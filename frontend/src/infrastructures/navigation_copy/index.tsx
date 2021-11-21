b82b5404d00cd42b73be97c54fec3973522d9598
Amir Pahadi <apahadi73@gmail.com>
2021-11-21 16:42:45 -0600

    app.js refactored

--- frontend/src/infrastructures/navigation_copy/index.tsx
+++ frontend/src/infrastructures/navigation_copy/index.tsx
@@ -4,7 +4,6 @@ import { NavigationContainer } from "@react-navigation/native";
 // import { AppNavigator } from "./app.navigator.tsx";
 import { AuthenticationContext } from "../../services/authentication/repo/Authentication.context";
 import AuthNavigator from "./authentication.navigator";
-import LoginScreen from "../../services/authentication/screens/LoginScreen.authentication";
 
 const NavigationHost = () => {
   // we need isAuthenticated to determine which navigation stack to show
@@ -13,7 +12,7 @@ const NavigationHost = () => {
 
   return (
     <NavigationContainer>
-      <LoginScreen />
+      <AuthNavigator />
     </NavigationContainer>
   );
 };
