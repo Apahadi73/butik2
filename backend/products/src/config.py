a1064ed3dcdb4c7c14617f38d8204dac62538a0f
Amir Pahadi <apahadi73@gmail.com>
2022-01-06 20:18:45 -0600

    ingress problem fixed

--- backend/products/src/config.py
+++ backend/products/src/config.py
@@ -16,10 +16,10 @@ POSTGRES_PORT = config("POSTGRES_PORT", cast=str, default="5432")
 POSTGRES_DB = config("POSTGRES_DB", cast=str,default="postgres")
 
 # TODO: DONT' FORGET TO UNDO THE CHANGES
-# DATABASE_URL = config(
-#   "DATABASE_URL",
-#   default=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-# )
+DATABASE_URL = config(
+  "DATABASE_URL",
+  default=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/postgres"
+# DATABASE_URL = "postgresql://postgres:password@localhost:5432/postgres"
 
